635b4e67de049fd63374f24c3343c1850fbb4568
guycart <89608772+guycart@users.noreply.github.com>
2022-02-27 17:38:16 -0500

    Updated movement scripts

--- John Lemon/Assets/Scripts/PlayerMovement.cs
+++ John Lemon/Assets/Scripts/PlayerMovement.cs
@@ -10,24 +10,22 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody m_Rigidbody;
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
-    
-    // Start is called before the first frame update
-    void Start()
+
+    void Start ()
     {
         m_Animator = GetComponent<Animator> ();
         m_Rigidbody = GetComponent<Rigidbody> ();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void FixedUpdate ()
     {
         float horizontal = Input.GetAxis ("Horizontal");
         float vertical = Input.GetAxis ("Vertical");
-
+        
         m_Movement.Set(horizontal, 0f, vertical);
         m_Movement.Normalize ();
 
-        bool hasHorizontalInput = !Math.Approximately (horizontal, 0f);
+        bool hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
         bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
         m_Animator.SetBool ("IsWalking", isWalking);
@@ -35,6 +33,7 @@ public class PlayerMovement : MonoBehaviour
         Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation (desiredForward);
     }
+
     void OnAnimatorMove ()
     {
         m_Rigidbody.MovePosition (m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
